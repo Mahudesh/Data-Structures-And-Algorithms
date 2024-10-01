c4c4b57714bf63dc8968b90c757dc3999ea5e5ff
Mahudesh <115523760+Mahudesh@users.noreply.github.com>
2024-10-01 10:28:30 +0530

    Update Minimum Number Of Days To Make M Bouquets.cpp

--- Binary Search/Minimum Number Of Days To Make M Bouquets.cpp
+++ Binary Search/Minimum Number Of Days To Make M Bouquets.cpp
@@ -1,46 +1,55 @@
+class Solution {
 public:
-  int isPossible(vector<int>& arr, int maxDays, int K, int M)
-  {
-      int flowers=0;
-      int cnt=0;
-      int bouquets=0;
-      for(int i=0;i<arr.size();i++)
-      {
-          if(arr[i]<=maxDays)
-          {
-              cnt++;
-          }
-          else
-          {
-              cnt=0;
-          }
-          if(cnt==K)
-          {
-              bouquets++;
-              cnt=0;
-          }
-      }
-      if(bouquets>=M)
-      return true;
-      return false;
-  }
-    int solve(int M, int K, vector<int> &bloomDay)
+int makeBokets(vector<int>&bloomDay, int m, int k,int days)
+{
+    int flowers=0;
+    int bok=0;
+    for(int i=0;i<bloomDay.size();i++)
     {
-      // Code here
-      if(M*K > bloomDay.size())
-      return -1;
-      int low=*min_element(bloomDay.begin(),bloomDay.end());
-      int high=*max_element(bloomDay.begin(),bloomDay.end());
-      while(low<=high)
-      {
-          int mid=(low+high)/2;
-          if(isPossible(bloomDay,mid,K,M))
-          {
-              high=mid-1;
-          }
-          else
-          {
-              low=mid+1;
-          }
-      }
-      return low;
+        if(bloomDay[i]<=days)
+        {
+            flowers++;
+        }
+        else
+        {
+            flowers=0;
+        }
+        if(flowers==k)
+        {
+            bok++;
+            flowers=0;
+        }
+    }
+    return bok;
+}
+int findMax(vector<int>&bloomDay)
+{
+    int maxi=INT_MIN;
+    for(auto it: bloomDay)
+    maxi=max(maxi,it);
+    return maxi;
+}
+    int minDays(vector<int>& bloomDay, int m, int k)
+     {
+        if((long long)m*(long long) k>bloomDay.size()) return -1;
+        int low=1;
+        int high=findMax(bloomDay);
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int bok=makeBokets(bloomDay,m,k,mid);
+            if(bok>=m)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+     //   if(low<m) return -1;
+        return low;
+    }
+};
+//1 10 3 10 2
+
