42f1e3a5eaf72d498959493d20bf651bdff1478e
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2024-10-11 10:33:38 +0530

    Update Set The Rightmost Unset Bit.cpp

--- Bit Manipulation/Set The Rightmost Unset Bit.cpp
+++ Bit Manipulation/Set The Rightmost Unset Bit.cpp
@@ -2,7 +2,7 @@
     int setBit(int n) 
     {
         // Write Your Code here
-        //It(n+1) Automatically Set Just Below The Last Unset Below As 1
+        //It(n+1) Automatically Set Just Below The Last Unset Below As 1 And Make All Bits Right To It's Get Toggled
         //So To Set The Bit Just Do The OR Operation
         return (n|n+1);
     }
