293a0776688dc1d64af9dd5810158b665985e962
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2024-10-15 16:05:15 +0530

    Create Subarray Product Less Than K.cpp

---
+++ Sliding Window & Two Pointer Approach/Subarray Product Less Than K.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    int numSubarrayProductLessThanK(vector<int>& nums, int k)
+     {
+        int l=0;
+        int r=0;
+        int cnt=0;
+        int prod=1;
+        while(r<nums.size())
+        {
+            prod=prod*nums[r];
+            while(prod>=k && l<=r)
+            {
+                prod=prod/nums[l];
+                l++;
+            }
+            cnt=cnt+(r-l)+1;
+            r++;
+        }
+        return cnt;
+    }
+};
