6129bea5ab902c6aca5cd60c6eb22968ff5b2430
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2024-10-28 15:38:12 +0530

    Create Reverse Star Triangle.cpp

---
+++ Pattern Printing/Reverse Star Triangle.cpp
@@ -0,0 +1,23 @@
+void nStarTriangle(int n) 
+{
+    // Write your code here.
+    int sp=0;
+    for(int i=n;i>=1;i--)
+    {
+        for(int j=1;j<=sp;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=(2*i)-1;j++)
+        {
+            cout<<"*";
+        }
+        for(int j=1;j<=sp;j++)
+        {
+            cout<<" ";
+        }
+        sp++;
+        cout<<endl;
+
+    }
+}
