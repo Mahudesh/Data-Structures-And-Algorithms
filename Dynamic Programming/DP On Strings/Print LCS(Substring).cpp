f8f13f290e17072a81b27b57a9062e6b99d83323
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2024-12-24 20:52:29 +0530

    Create Print LCS(Substring).cpp

---
+++ Dynamic Programming/DP On Strings/Print LCS(Substring).cpp
@@ -0,0 +1,56 @@
+olution {
+  public:
+    int longestCommonSubstr(string& s1, string& s2) 
+    {
+        // your code here
+        int n=s1.length();
+        int m=s2.length();
+        vector<vector<int>>dp(n+1, vector<int>(m+1,0));
+        for(int j=0;j<=m;j++)
+        {
+            dp[0][j]=0;
+        }
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=0;
+        }
+        int maxi=0;
+        int r=0,c=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    //If It Matches Check For Previosuly Any Consecutive Matches
+                    //1+ For Current Character With If Any Previous Matches
+                    dp[i][j]=1+dp[i-1][j-1];
+                    // maxi=max(maxi,dp[i][j]);
+                    if(maxi<dp[i][j])
+                    {
+                        maxi=dp[i][j];
+                        r=i;
+                        c=j;
+                    }
+                }
+                else
+                {
+                    //Since It Is Not Matching Then There Comes A Disrupt
+                    //A Disturbance Or Non - Matched Character Arises
+                    //Hence It Does Not Depend Upon Previous Result
+                    //If Any Disrupt Occurs Then Make It As 0
+                    dp[i][j]=0;
+                }
+            }
+        }
+        string ans;
+        int t=maxi;
+        while(t-- > 0)
+        {
+            ans.push_back(s1[r-1]);
+            r--;
+        }
+        reverse(ans.begin(),ans.end());
+        cout<<ans;
+        return maxi;
+    }
