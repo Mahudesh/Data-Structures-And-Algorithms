9a41e271a3c82fc0e1d22376eb14b24b7cecc04b
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2025-01-01 11:24:48 +0530

    Update Longest Increasing Subsequence.cpp

--- Dynamic Programming/DP On LIS/Longest Increasing Subsequence.cpp
+++ Dynamic Programming/DP On LIS/Longest Increasing Subsequence.cpp
@@ -21,25 +21,20 @@ int helper(vector<int>& nums, int ind, int prev_ind, vector<vector<int>>& dp)
         // return helper(nums,0,-1,dp);
 
         //Tabulation Approach
-        //Bottom Up Approach
-        vector<vector<int>>dp(n+1, vector<int>(n+1,0));
+        //Bottom Up Approach  vector<vector<int>>dp(n+1, vector<int>(n+1,0));
 
-        for(int prev=0;prev<=n;prev++)
-        dp[n][prev]=0;
-
-        for(int ind=n-1;ind>=0;ind--)
+    for(int ind=n-1;ind>=0;ind--)
+    {
+        for(int prev_ind=ind-1;prev_ind>=-1;prev_ind--)
         {
-            for(int prev_ind = -1;prev_ind<=n-1;prev_ind++)
+            int notTake=0+dp[ind+1][prev_ind+1];
+            int take=0;
+            if(prev_ind==-1 || arr[ind]>arr[prev_ind])
             {
-                int notTake=0+dp[ind+1][prev_ind+1];
-                int take=0;
-                if(prev_ind== -1 || nums[ind]>nums[prev_ind])
-                {
-                    take=1+dp[ind+1][ind+1];
-                }
-                dp[ind][prev_ind+1]=max(notTake,take);
+                take=1+dp[ind+1][ind+1];
             }
+            dp[ind][prev_ind+1]=max(notTake,take);
         }
-        return dp[0][0];
     }
+    return dp[0][0];
 };
