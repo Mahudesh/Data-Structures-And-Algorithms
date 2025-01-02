dc1fae459ff1a54a9dcb973eaf077b225c050f26
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2025-01-02 20:17:59 +0530

    Create Matrix Chain Multiplication.cpp

---
+++ Dynamic Programming/Partition DP/Matrix Chain Multiplication.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h> 
+
+int helper(int i, int j, vector<int>& arr, vector<vector<int>>& dp)
+{
+    if(i==j)
+    return 0;
+
+    int mini=1e9;
+
+    if(dp[i][j]!=-1)
+    return dp[i][j];
+
+    for(int k=i;k<=j-1;k++)
+    {
+        int operations=arr[i-1] * arr[k] * arr[j] + 
+                        helper(i,k,arr,dp) + helper(k+1,j,arr,dp);
+
+        mini=min(mini,operations);
+    }
+    return dp[i][j]=mini;
+}
+int matrixMultiplication(vector<int> &arr, int N)
+{
+    // Write your code here.
+    // vector<vector<int>>dp(N, vector<int>(N,-1));
+    // return helper(1,N-1,arr,dp);
+    vector<vector<int>>dp(N, vector<int>(N,0));
+
+    //Tabulation Approach
+    //Bottom Up Approach
+
+    for(int i=1;i<N;i++)
+    {
+        dp[i][i]=0;
+    }
+
+    for(int i=N-1;i>=1;i--)
+    {
+        for(int j=i+1;j<N;j++)
+        {
+            int mini=1e9;
+            for(int k=i;k<=j-1;k++)
+            {
+                int operations=arr[i-1] * arr[k] * arr[j] + 
+                        dp[i][k] + dp[k+1][j];
+
+                 mini=min(mini,operations);
+            }
+                 dp[i][j]=mini;
+        }
+    }
+    return dp[1][N-1];
+}
