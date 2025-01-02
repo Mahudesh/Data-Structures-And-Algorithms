a13f56095fa7bf8ef8622b9714136e63e39b4e5d
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2025-01-02 13:49:06 +0530

    Create Longest Continuous Increasing Subsequence.cpp

---
+++ Dynamic Programming/DP On LIS/Longest Continuous Increasing Subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) 
+    {
+        // if(nums.size())
+        int cnt=1;
+        int ans=1;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt=1;
+            }
+            ans=max(ans,cnt);
+        }
+        return ans;
+    }
+};
