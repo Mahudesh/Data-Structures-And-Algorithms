3c27471fa08eba24fd72de33b6ce03237da09934
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2025-01-12 19:51:12 +0530

    Update Maximum Sum Rectangle.cpp

--- Dynamic Programming/DP On Rectangles/Maximum Sum Rectangle.cpp
+++ Dynamic Programming/DP On Rectangles/Maximum Sum Rectangle.cpp
@@ -1,34 +1,81 @@
-#include <bits/stdc++.h> 
-int kadanesAlgorithm(vector <int>& arr)
+// You are using GCC
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> kadanesAlgorithm(vector<int>&arr)
 {
-	int sum=0;
-	int maxSum=INT_MIN;
-	for(int i=0;i<arr.size();i++)
-	{
-		sum+=arr[i];
-		maxSum=max(maxSum,sum);
-		if(sum<0)
-		sum=0;
-	}
-	return maxSum;
+    int sum=0;
+    int maxSum=INT_MIN;
+    int tempStart=0;
+    int start=0;
+    int end=0;
+    for(int i=0;i<arr.size();i++)
+    {
+        sum+=arr[i];
+        // maxSum=max(maxSum,sum);
+        if(maxSum<sum)
+        {
+            maxSum=sum;
+            start=tempStart;
+            end=i;
+        }
+        if(sum<0)
+        {
+            sum=0;
+            tempStart=i+1;
+        }
+    }
+    vector<int>temp(3);
+    temp[0]=maxSum;
+    temp[1]=start;
+    temp[2]=end;
+    return temp;
 }
-int maxSumRectangle(vector<vector<int>>& arr, int n, int m)
+int main()
 {
-	// write your code here
-	int maxSum=INT_MIN;
-	for(int topRow=0;topRow<n;topRow++)
-	{
-		vector<int>temp(m,0);
-		for(int r=topRow;r<n;r++)
-		{
-			for(int c=0;c<m;c++)
-			{
-				temp[c]=temp[c]+arr[r][c];
-			}
-				int sum=kadanesAlgorithm(temp);
-				if(maxSum<sum)
-				maxSum=sum;
-		}
-	}
-	return maxSum;
+    int n;
+    int m;
+    cin>>n>>m;
+    vector<vector<int>>arr(n, vector<int>(m));
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    int maxSum=INT_MIN;
+    int maxLeft;
+    int maxRight;
+    int maxTop;
+    int maxBottom;
+    for(int topRow=0;topRow<n;topRow++)
+    {
+            vector<int>sumArray(m,0);
+        for(int bottomRow=topRow;bottomRow<n;bottomRow++)
+        {
+            for(int col=0;col<m;col++)
+            {
+                sumArray[col]+=arr[bottomRow][col];
+            }
+            
+            vector<int>temp=kadanesAlgorithm(sumArray);
+            int sum=temp[0];
+            int left=temp[1];
+            int right=temp[2];
+            if(maxSum<sum)
+            {
+                maxSum=sum;
+                maxLeft=left;
+                maxRight=right;
+                maxTop=topRow;
+                maxBottom=bottomRow;
+                
+            }
+        }
+    }
+    
+    // if(maxSum<)
+    cout<<"from row "<<maxTop<<" to "<<"row "<<maxBottom<<endl;
+    cout<<"from col "<<maxLeft<<" to "<<"col "<<maxRight<<endl;
+    cout<<maxSum;
 }
