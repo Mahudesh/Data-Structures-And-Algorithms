5495a472ac266b8ecdb2988dfef1c6afabb752e6
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2025-01-18 15:27:55 +0530

    Update Minimum Add To Make Paranthesis Valid.cpp

--- Dynamic Programming/DP On Strings/Minimum Add To Make Paranthesis Valid.cpp
+++ Dynamic Programming/DP On Strings/Minimum Add To Make Paranthesis Valid.cpp
@@ -1,38 +1,62 @@
+// class Solution {
+// public:
+//     int minAddToMakeValid(string s) 
+//     {
+//         //Always Try To Keep What I Can Not Touch
+//         //Here I Cannot Touch The Valid Paranthesis
+//         //If A Paranthesis Is Valid Then It Uses 2 Brackets
+//         //So Make Sure U Multiply The Valid Paranthesis By Two
+//         int cnt=0;
+//         stack<char>st;
+//         for(int i=0;i<s.length();i++)
+//         {
+//             if(s[i]=='(')
+//             {
+//                 st.push(s[i]);
+//             }
+//             else
+//             {
+//                 if(st.empty())
+//                 continue;
+
+//                 if(st.top()=='(' && s[i]==')')
+//                 {
+//                     st.pop();
+//                     cnt++;
+//                 }
+//                 else
+//                 continue;
+                
+//             }
+//         }
+//         //If A Paranthesis Is A Valid Then It Will Contain Two Valid Brackets
+//         //So For Every Valid Multiply It By Two
+//         //Subtract The Total Length - Valid Bracket Will Give You Minimum Insertions
+
 class Solution {
 public:
     int minAddToMakeValid(string s) 
     {
-        //Always Try To Keep What I Can Not Touch
-        //Here I Cannot Touch The Valid Paranthesis
-        //If A Paranthesis Is Valid Then It Uses 2 Brackets
-        //So Make Sure U Multiply The Valid Paranthesis By Two
-        int cnt=0;
-        stack<char>st;
+        int valid=0;
+        int open=0;
+        int close=0;
         for(int i=0;i<s.length();i++)
         {
             if(s[i]=='(')
             {
-                st.push(s[i]);
+                open++;
             }
-            else
+            else if(s[i]==')' && open>0)
             {
-                if(st.empty())
-                continue;
-
-                if(st.top()=='(' && s[i]==')')
-                {
-                    st.pop();
-                    cnt++;
-                }
-                else
-                continue;
-                
+                //Cancel One Opening Bracket For One Closing Bracket (If There Exists A Opening Bracket)
+                open--;
+                valid++;
             }
         }
-        //If A Paranthesis Is A Valid Then It Will Contain Two Valid Brackets
-        //So For Every Valid Multiply It By Two
-        //Subtract The Total Length - Valid Bracket Will Give You Minimum Insertions
-        int valid=cnt*2;
-        return s.length()-valid;
+        return s.length()-2*valid;
     }
 };
+//         int valid=cnt*2;
+//         return s.length()-valid;
+//     }
+// };
