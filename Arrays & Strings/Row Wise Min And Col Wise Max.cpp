3b8f9f5e0416c695cc580c4710ac9df6524bccf4
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2025-01-18 11:07:51 +0530

    Update Row Wise Min And Col Wise Max.cpp

--- Arrays & Strings/Row Wise Min And Col Wise Max.cpp
+++ Arrays & Strings/Row Wise Min And Col Wise Max.cpp
@@ -1,28 +1,58 @@
-#include <bits/stdc++.h> 
-vector <int> chuninNinja(int n , int m , vector < vector < int > > arr)
-{
-   // Write your code here.
-   vector<int>ans;
-   vector<int>rowMin(n,INT_MAX);
-   vector<int>colMax(m,INT_MIN);
-   for(int i=0;i<n;i++)
-   {
-      for(int j=0;j<m;j++)
-      {
-         rowMin[i]=min(rowMin[i],arr[i][j]);
-         colMax[j]=max(colMax[j],arr[i][j]);
-      }
-   }
-   for(int i=0;i<n;i++)
-   {
-      for(int j=0;j<m;j++)
-      {
-         if(arr[i][j]==rowMin[i] && arr[i][j]==colMax[j])
-         ans.push_back(arr[i][j]);
-      }
-   }
-   return ans;
-}
-// 3 4 5
-// 2 7 6
-// 1 2 4
+class Solution {
+public:
+// bool isMin(int n, int m, vector<vector<int>>& matrix, int ele, int rowIndex)
+// {
+//     for(int j=0;j<m;j++)
+//     {
+//         if(ele>matrix[rowIndex][j])
+//         return false;
+//     }
+//     return true;
+// }
+    vector<int> luckyNumbers(vector<vector<int>>& matrix) 
+    {
+        
+        vector<int>ans;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<int>rowMin(n,INT_MAX);
+        vector<int>colMax(m,INT_MIN);
+
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                rowMin[i]=min(rowMin[i],matrix[i][j]);
+                colMax[j]=max(colMax[j],matrix[i][j]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==rowMin[i] && colMax[j]==matrix[i][j])
+                ans.push_back(matrix[i][j]);
+            }
+        }
+        // for(int j=0;j<m;j++)
+        // {
+        //     int rowIndex=-1;
+        //     int maxi=INT_MIN;
+        //     for(int i=0;i<n;i++)
+        //     {
+        //         if(maxi<matrix[i][j])
+        //         {
+        //             maxi=matrix[i][j];
+        //             rowIndex=i;
+        //         }
+        //     }
+        //     if(isMin(n,m,matrix,maxi,rowIndex))
+        //     ans.push_back(maxi);
+        // }
+        return ans;
+    }
+};
+// 3 7 8
+// 9 11 13
+// 15 16 17
