21bb7ce5a10830cbb4fc6ff4234672399f791265
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2025-02-27 17:33:59 +0530

    Update Is Bipartite Graph.cpp

--- Graph/Is Bipartite Graph.cpp
+++ Graph/Is Bipartite Graph.cpp
@@ -45,5 +45,57 @@ bool Check(int start,vector<vector<int>>& adj, vector<int>& colored)
         return true;
     }
 };
+
+//DFS Solution
+bool dfs(int start,int color, vector<vector<int>>& adj, vector<int>& colored)
+{
+	colored[start]=color;
+
+	for(auto adjacent : adj[start])
+	{
+		if(colored[adjacent]==-1)
+		{
+			if(dfs(adjacent,!color,adj,colored)==false)
+			return false;
+		}
+		else if(colored[adjacent]!=-1)
+		{
+			 if(colored[adjacent]==colored[start])
+			 return false;
+		}
+	}
+	return true;
+}
+bool isGraphBirpatite(vector<vector<int>> &edges) 
+{
+	// Write your code here.
+	int n=edges.size();
+	vector<vector<int>>adj(n);
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<edges[i].size();j++)
+		{
+			if(edges[i][j]==1)
+			{
+				int u=i;
+				int v=j;
+				adj[u].push_back(v);
+				adj[v].push_back(u);
+			}
+		}
+
+
+	}
+	vector<int>colored(n,-1);
+	for(int i=0;i<n;i++)
+	{
+		if(colored[i]==-1){
+		if(!dfs(i,0,adj,colored))
+		return false;
+		}
+		
+	}
+	return true;
+}
 // 1 2 3
 // 0 
