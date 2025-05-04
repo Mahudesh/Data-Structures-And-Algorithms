bdea7f529c51829592b4e42623877bd8e776b14a
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2025-05-04 10:49:01 +0530

    Update Jump Game II.cpp

--- Greedy Algorithms/Jump Game II.cpp
+++ Greedy Algorithms/Jump Game II.cpp
@@ -1,24 +1,63 @@
-class Solution {
-public:
-    int jump(vector<int>& nums) 
+// int helper(int ind, vector<int>& arr, int n)
+// {
+//     if(ind>=n-1)
+//     return 0;
+//     if(arr[ind]==0)
+//     return 1e9;
+//     int mini=1e9;
+//     int ele=arr[ind];
+//     for(int i=1;i<=ele;i++)
+//     {
+//         if(ind+i<n)
+//         {
+//             int jumps=1+helper(ind+i,arr,n);
+//             mini=min(mini,jumps);
+//         }
+//     }
+//     return mini;
+// }
+int minJumps(vector<int> &arr,int n) 
+{
+
+    int curr=0;
+    int maxIndex=0;
+    int jumps=0;
+    for(int i=0;i<n;i++)
     {
-      int ans=0;
-      int farthest=0;
-      int curr=0;
-      int jumps=0;
-      for(int i=0;i<nums.size();i++)
-      {
-        farthest=max(farthest,nums[i]+i);
-        if(curr>=nums.size()-1)
-        {
-            return jumps;
-        }
-        if(curr==i)
+        maxIndex=max(maxIndex,i+arr[i]);
+        if(curr>=n-1)
+        return jumps;
+        if(i==curr)
         {
             jumps++;
-            curr=farthest;
+            curr=maxIndex;
         }
-      }  
-      return 0;
     }
-};
+    return -1;
+    // Write your code here.
+    // vector<int>dp(n,0);
+
+    // dp[n-1]=0;
+    // for(int ind=n-2;ind>=0;ind--)
+    // {
+    //     int ele=arr[ind];
+    //     int mini=1e9;
+    //     for(int i=1;i<=ele;i++)
+    //     {
+    //         if(ind+i<n)
+    //         {
+    //             int jumps=1+dp[ind+i];
+    //             mini=min(mini,jumps);
+    //         }
+
+    //     }
+    //     dp[ind]=mini;
+    // }
+    // if(dp[0]==1e9)
+    // return -1;
+    // return dp[0];
+    // int a=helper(0,arr,n);
+    // if(a==1e9)
+    // return -1;
+    // return a;
+}
