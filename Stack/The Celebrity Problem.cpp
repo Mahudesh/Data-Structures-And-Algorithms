cff247bb2fa77cdc663006e2663352a14e75ab46
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2025-05-07 15:25:01 +0530

    Update The Celebrity Problem.cpp

--- Stack/The Celebrity Problem.cpp
+++ Stack/The Celebrity Problem.cpp
@@ -1,25 +1,57 @@
- public:
+Solution {
+  public:
     // Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& mat)
     {
         // code here
-        vector<int>iknow(mat.size());
-        vector<int>known(mat.size());
-        for(int i=0;i<mat.size();i++)
+        // vector<int>iknow(mat.size());
+        // vector<int>known(mat.size());
+        // for(int i=0;i<mat.size();i++)
+        // {
+        //     for(int j=0;j<mat[i].size();j++)
+        //     {
+        //         if(mat[i][j]==1)
+        //         {
+        //             known[j]=known[j]+1;
+        //             iknow[i]=iknow[i]+1;
+        //         }
+        //     }
+        // }
+        // for(int i=0;i<known.size();i++)
+        // {
+        //     if(known[i]==mat.size()-1 && iknow[i]==0)
+        //     return i;
+        // }
+        // return -1;
+        
+        //Optimized
+        int top=0;
+        int bottom=mat.size()-1;
+        
+        while(top<bottom)
         {
-            for(int j=0;j<mat[i].size();j++)
+            if(mat[top][bottom]==1)
+            top++;
+            else if(mat[bottom][top]==1)
+            bottom--;
+            else
             {
-                if(mat[i][j]==1)
-                {
-                    known[j]=known[j]+1;
-                    iknow[i]=iknow[i]+1;
-                }
+                top++;
+                bottom--;
             }
         }
-        for(int i=0;i<known.size();i++)
-        {
-            if(known[i]==mat.size()-1 && iknow[i]==0)
-            return i;
-        }
+        if(top>bottom)
         return -1;
+        for(int j=0;j<mat.size();j++)
+        {
+            if(top!=j)
+            {
+                if(mat[top][j]==0 && mat[j][top]==1)
+                continue;
+                else
+                return -1;
+            }
+        }
+        return top;
     }
+};
