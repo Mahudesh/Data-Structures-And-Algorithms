139dfd7ec9d23b794aed29dcdbceac78b62dc795
Mahudesh M <115523760+Mahudesh@users.noreply.github.com>
2025-05-19 20:09:08 +0530

    Update Maximum Size Rectangle.cpp

--- Stack/Maximum Size Rectangle.cpp
+++ Stack/Maximum Size Rectangle.cpp
@@ -1,69 +1,57 @@
-#include<stack>
-int findHistogram(vector<int>&arr)
+class Solution {
+public:
+int f(vector<int>& arr)
 {
-	int n=arr.size();
-	vector<int>prevSmaller(n,0);
-	vector<int>nextSmaller(n,n-1);
-	stack<int>st;
-	for(int i=0;i<n;i++)
-	{
-		while(!st.empty() && arr[st.top()]>=arr[i])
-		{
-			st.pop();
-		}
-		if(!st.empty())
-		{
-			prevSmaller[i]=st.top()+1;
-		}
-		st.push(i);
-	}
-	while(!st.empty())
-	{
-		st.pop();
-	}
-	for(int i=n-1;i>=0;i--)
-	{
-		while(!st.empty() && arr[st.top()]>=arr[i])
-		{
-			st.pop();
-		}
-		if(!st.empty())
-		{
-			nextSmaller[i]=st.top()-1;
-		}
-		st.push(i);
-	}
-	int area=0;
-	for(int i=0;i<n;i++)
-	{
-		int width=nextSmaller[i]-prevSmaller[i]+1;
-		int height=arr[i];
-		area=max(area,width*height);
-	}
-	return area;
-
+    stack<int>st;
+    int n=arr.size();
+    vector<int>prevSmaller(n,0);
+    vector<int>nextSmaller(n,n-1);
+    for(int i=0;i<arr.size();i++)
+    {
+        while(!st.empty() && arr[st.top()]>=arr[i])
+        st.pop();
+        if(!st.empty())
+        prevSmaller[i]=st.top()+1;
+        st.push(i);
+    }
+    while(!st.empty())
+    st.pop();
+    for(int i=n-1;i>=0;i--)
+    {
+        while(!st.empty() && arr[st.top()]>=arr[i])
+        st.pop();
+        if(!st.empty())
+        nextSmaller[i]=st.top()-1;
+        st.push(i);
+    }
+    int maxi=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        int h=arr[i];
+        int w=nextSmaller[i]-prevSmaller[i]+1;
+        maxi=max(maxi,h*w);
+    }
+    return maxi;
 }
-int maximalAreaOfSubMatrixOfAll1(vector<vector<int>> &mat, int n, int m)
-{
-	// Write your code here.
-	vector<vector<int>> prefixSum(n, vector<int>(m));
-	for(int j=0;j<m;j++)
-	{
-		int sum=0;
-		for(int i=0;i<n;i++)
-		{
-			sum+=mat[i][j];
-			if(mat[i][j]==0)
-			{
-				sum=0;
-			}
-			prefixSum[i][j]=sum;
-		}
-	}
-	int maxi=0;
-	for(int i=0;i<prefixSum.size();i++)
-	{
-		maxi=max(maxi,findHistogram(prefixSum[i]));
-	}
-	return maxi;
-}
+    int maximalRectangle(vector<vector<char>>& matrix) 
+    {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        // vector<vector<int>>arr(n, vector<int>(m+1,0));
+        vector<int>arr(m,0);
+        int maxi=INT_MIN;
+        // arr=matrix[0];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]=='1')
+                arr[j]++;
+                else
+                arr[j]=0;
+            }
+            maxi=max(maxi,f(arr));
+        }
+        return maxi;
+    }
+};
